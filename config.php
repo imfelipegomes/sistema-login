59323f76778921f5b9fcccf1110f907309131f71
imfelipegomes <felipe.agomesdelima@gmail.com>
2025-04-18 14:39:10 -0300

    Finalização do projeto do Sistema de Login com Bootstrap, PHP e MySQL

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  define('HOST', 'localhost');
+  define('USER', 'root');
+  define('PASS', 'imfelipegomes');
+  define('BASE', 'sislogin');
+
+  $conn = new MySQLi(HOST, USER, PASS, BASE);
--- login.php
+++ login.php
@@ -2,7 +2,7 @@
     session_start();
 
       if(empty($_POST) or (empty($_POST["usuario"]) or (empty($_POST["senha"])))){
-      print "<script>location.href="index.php";</script>";
+      print "<script>location.href='index.php';</script>";
       }
 
       include("config.php");
@@ -24,8 +24,8 @@ if($qtd > 0){
     $_SESSION["usuario"] = $usuario;
     $_SESSION["nome"] = $row->nome;
     $_SESSION["tipo"] = $row->tipo;
-    print "<script>location.href='dashoard.php';</script>";
+    print "<script>location.href='dashboard.php';</script>";
 }else{
-    print "<script>alert('Usuário e/ou senha incorreto(s)');
+    print "<script>alert('Usuário e/ou senha incorreto(s)');</script>";
     print "<script>location.href='index.php';</script>";
 }
